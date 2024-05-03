bfc3f1c5d00264a5e52054a4329fc1e6a229e5ec
KarimCarl <cndungu109@gmail.com>
2024-05-03 21:53:32 +0300

    Created variables in JavaScript file to select all elements in the html file

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// constant variables for all number buttons
+const numberButtons = document.querySelectorAll('[data-number]')
+
+// constant variables for all operation buttons like *,+,-,/
+const operationButtons = document.querySelectorAll('[data-operation]')
+
+// constant variables for equals buttons
+const equalsButton = document.querySelector('[data-equals]')
+
+// constant variables for delete button
+const deleteButton = document.querySelector('[data-delete]')
+
+// constant variables for all clear button
+const allClearButton = document.querySelector('[data-all-clear]')
+
+// constant variables for previous Operand Text Element where previous text goes
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+
+// constant variables for equals buttons
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
